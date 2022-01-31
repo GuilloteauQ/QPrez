7a20949b313c525f309564c92f8df2fa00846bdf
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2022-01-31 09:51:42 +0100

    try to link sty files

--- R/qprez.R
+++ R/qprez.R
@@ -6,10 +6,16 @@ qprez <- function(...) {
     if (template == "") {
         stop("Could not find the template")
     }
-    sty_shark = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamercolorthemeshark.sty", package = "qprez")
-    sty_chamfered = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamerinnerthemechamfered.sty", package = "qprez")
-    sty_wuerzburg = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamerouterthemewuerzburg.sty", package = "qprez")
-    sty_cbf = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "cbfcolors.sty", package = "qprez")
+    file_sty_shark = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamercolorthemeshark.sty", package = "qprez")
+    file_sty_chamfered = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamerinnerthemechamfered.sty", package = "qprez")
+    file_sty_wuerzburg = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamerouterthemewuerzburg.sty", package = "qprez")
+    file_sty_cbf = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "cbfcolors.sty", package = "qprez")
+
+    sty_shark = paste("\\usepackage{", file_sty_shark, "}", sep="")
+    sty_chamfered = paste("\\usepackage[outline]{", file_sty_chamfered, "}", sep="")
+    sty_wuerzburg = paste("\\usepackage[]{", file_sty_chamfered, "}", sep="")
+    sty_cbf = paste("\\usepackage{", file_sty_cbf, "}", sep="")
+
     includes <- list()
     includes$in_header <- c(sty_shark, sty_chamfered, sty_wuerzburg, sty_cbf)
 	rmarkdown::beamer_presentation(template = template, includes = includes, ...)
