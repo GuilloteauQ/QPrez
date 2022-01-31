06ecfa7a83a3d1f19742765157ecd24ca2c60a42
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2022-01-31 09:34:52 +0100

    includes

--- R/qprez.R
+++ R/qprez.R
@@ -6,5 +6,11 @@ qprez <- function(...) {
     if (template == "") {
         stop("Could not find the template")
     }
-	rmarkdown::beamer_presentation(template = template, ...)
+    sty_shark = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamercolorthemeshark.sty", package = "qprez")
+    sty_chamfered = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamerinnerthemechamfered.sty", package = "qprez")
+    sty_wuerzburg = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "beamerouterthemewuerzburg.sty", package = "qprez")
+    sty_cbf = system.file("rmarkdown", "templates", "qprez", "resources", "sty", "cbfcolors.sty", package = "qprez")
+    includes <- list()
+    includes$in_header <- c(sty_shark, sty_chamfered, sty_wuerzburg, sty_cbf)
+	rmarkdown::beamer_presentation(template = template, includes = includes, ...)
 }
